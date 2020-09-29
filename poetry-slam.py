b5627b0571e3608b5904065ed3c3b0e57cfaea22
Luis <LVentura335@Gmail.com>
2020-09-29 11:23:05 -0700

    first commit

---
+++ poetry-slam.py
@@ -0,0 +1,24 @@
+
+'''
+get_file_lines receives a text file as an argument, creates and returns
+a list of strings called lineList using the lines from the filename that are
+separated by a newline, or backslash n.
+'''
+
+
+def get_file_lines(filename):
+    lineList = [line.rstrip('\n') for line in open(filename)]
+    return lineList
+
+
+'''
+lines_printed_backwards receives a list of lines, expectedly the
+lineList returned from the get_file_lines function, and uses the
+built in reversed() function to print each line from the list
+starting from the last line.
+'''
+
+
+def lines_printed_backwards(lineList):
+    for line in reversed(lineList):
+        print(line)
